6366a88616691877c3b385e4e49e1b6c1288642f
LukasChadourne-IUT <lukas.chadourne@iut-rodez.fr>
2026-07-23 22:06:52 +0200

    CHRONO_GUARD : IA Roaming (déplacements sans détection) fonctionnelle

--- Scripts/chrono_guard.gd
+++ Scripts/chrono_guard.gd
@@ -2,10 +2,6 @@ extends CharacterBody2D
 
 @onready var anim_running_2d : AnimatedSprite2D = $AnimatedSprite2D
 @export var patrol_points: Array[Marker2D] = []
-#Area of the chrono-guard detection
-@onready var area_2d: Area2D = $Area2D
-
-
 var current_point_index = 0
 var player : Node2D = null
 var speed = 350.0
@@ -15,8 +11,7 @@ func _ready() -> void:
 	if patrol_points.size() == 0:
 		print("No patrol points assigned")
 		
-	area_2d.body_entered.connect(_on_player_detected)
-	area_2d.body_exited.connect(_on_player_lost)
+	print(patrol_points[1].position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,22 +22,8 @@ func roaming() -> void:
 	if patrol_points.size() > 0 :
 		var target = patrol_points[current_point_index].position
 		velocity = (target - position).normalized() * speed
-		
-		if position.distance_to(target) <20.0:
-			print("i'm in")
+		print(position.distance_to(target))
+		if position.distance_to(target) < 65:
 			current_point_index += 1
 			if current_point_index >= patrol_points.size():
 				current_point_index = 0
-
-#func _on_timer_timeout() -> void:
-#	direction *= -1
-#	anim_running_2d.flip_h = !anim_running_2d.flip_h
-
-
-func _on_player_detected(body: Node2D) -> void:
-	if body.name == "Player" :
-		player = body
-
-func _on_player_lost(body : Node2D) -> void:
-	if body.name == "Player" :
-		player = null
