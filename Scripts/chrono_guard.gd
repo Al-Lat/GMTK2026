c36ca865c5d98f3568971ef859a3974672a25007
LukasChadourne-IUT <lukas.chadourne@iut-rodez.fr>
2026-07-24 15:33:07 +0200

    CHRONO-GUARD : mode chasse termine

--- Scripts/chrono_guard.gd
+++ Scripts/chrono_guard.gd
@@ -2,28 +2,55 @@ extends CharacterBody2D
 
 @onready var anim_running_2d : AnimatedSprite2D = $AnimatedSprite2D
 @export var patrol_points: Array[Marker2D] = []
+@onready var detection_area: Area2D = $detection_area
+
 var current_point_index = 0
 var player : Node2D = null
-var speed = 350.0
+var speed = 250.0
+var orientation = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if patrol_points.size() == 0:
 		print("No patrol points assigned")
-		
-	print(patrol_points[1].position)
+	
+	detection_area.body_entered.connect(on_player_detected)
+	detection_area.body_exited.connect(on_player_lost)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	roaming()
+	if player:
+		speed = 350
+		chase()
+	else:	
+		speed = 250
+		roaming()
 	move_and_slide()
-	
+
 func roaming() -> void:
 	if patrol_points.size() > 0 :
 		var target = patrol_points[current_point_index].position
 		velocity = (target - position).normalized() * speed
-		print(position.distance_to(target))
 		if position.distance_to(target) < 65:
 			current_point_index += 1
+			scale.x *=-1
 			if current_point_index >= patrol_points.size():
 				current_point_index = 0
+			print(current_point_index)	
+
+func chase() -> void:
+	velocity = (player.global_position - global_position).normalized() * speed
+	if (player.global_position.x - global_position.x) * orientation < 0 : 
+		scale.x *=-1
+		orientation *= -1
+
+func on_player_detected(body: Node2D) -> void:
+	if body.name == "Player" : 
+		player = body
+		print("Objectif : suivre ", body.name)
+
+func on_player_lost(body: Node2D) -> void:
+	if body.name == "Player":
+		player = null
+		print ("Perte de détection")
