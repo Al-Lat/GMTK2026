649ec07fa0446b712cdb1b7a95b512e34f93c94f
Al-Lat <a.laouat@outlook.fr>
2026-07-24 19:58:17 +0200

    Attack auto repris et fonctionelle

--- AttackBlueShot.gd
+++ AttackBlueShot.gd
@@ -1,5 +1,8 @@
 class_name AttackBlueShot extends Attack
 
+var is_allowed_fire = true
+var cooldown = 0.5
+
 func _init():
 	self.attack_type = AttackType.RANGED
 	self.attack_scene = preload("res://Scenes/AttackScenes/blue_shot.tscn")
@@ -8,11 +11,16 @@ func isInputPatternOk()->bool:
 	return Input.is_action_pressed("player_attack_ranged")
 
 func summon(entity) -> void:
-		var projectile = self.attack_scene.instantiate()
-		entity.add_child(projectile)
-		projectile.global_position = entity.global_position + entity.direction_to_mouse * entity.RANGED_ATTACK_OFFSET
-		projectile.rotation = entity.direction_to_mouse.angle()
-		projectile.direction = entity.direction_to_mouse.normalized()
+		if is_allowed_fire:
+			is_allowed_fire = false
+			var projectile = self.attack_scene.instantiate()
+			entity.add_child(projectile)
+			projectile.global_position = entity.global_position + entity.direction_to_mouse * entity.RANGED_ATTACK_OFFSET
+			projectile.rotation = entity.direction_to_mouse.angle()
+			projectile.direction = entity.direction_to_mouse.normalized()
+			entity.get_tree().create_timer(cooldown).timeout.connect(allow_fire)
 	
-
+func allow_fire():
+	is_allowed_fire = true
+	
 	
--- Scripts/AttackScripts/blue_slash.gd
+++ Scripts/AttackScripts/blue_slash.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 
 @onready var colision = $attack_colision2d
-#@onready var animation = $attack_animation
+@onready var anim_player =$AnimationPlayer 
+@onready var animation = $playerAnimation
 var parent:CharacterBody2D
 
 var player_singleton = PlayerSingleton
@@ -13,24 +14,33 @@ var mainAnimation:AnimatedSprite2D
 func _ready() -> void:
 	parent = $".."
 	player = player_singleton.player
+	animation.flip_h = (player.facing_direction == -1)
+	animation.position.x += player.facing_direction * 16
+	
 	mainAnimation =  parent.get_node("playerAnimation")
+	
 	print("position avant : ",mainAnimation.position.x)
-	mainAnimation.position.x += player.facing_direction * 16
-	mainAnimation.play("blue_slash")
+	mainAnimation.visible = false
+	anim_player.play("new_animation")
 	print("position apres : ",mainAnimation.position.x)
+	
 	body_entered.connect(_on_body_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	mainAnimation.position.x *= player.facing_direction
+#func _process(delta: float) -> void:
+	#animation.position.x *= player.facing_direction
 
 #TODO traitement collision
 func _on_body_entered(body: Node) -> void:
 	print("blue_slash : Touché : ", body.name)
 
 
-func _animation_finished() -> void:
+
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	print("blue_slash : self delete")
-	mainAnimation.position.x = 0
+	mainAnimation.visible = true
 	queue_free()
