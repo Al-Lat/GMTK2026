f335d360e6febb5a2cdc1be6b4cc4d3b596962ff
Nathan Capelle <nathan.capelle@iut-rodez.fr>
2026-07-25 10:51:03 +0200

    fix du portail

--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -2,22 +2,6 @@ extends Node2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-var in_portal = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animated_sprite.play("default")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if in_portal && Input.is_action_pressed("interaction"):
-		get_tree().change_scene_to_file("res://Scenes/map.tscn")
-
-
-func _on_body_entered(body: Node2D) -> void:
-	in_portal = true
-
-
-func _on_body_exited(body: Node2D) -> void:
-	in_portal = false
